235b59ff93a515951d21084ad356d09a3d75bca9
Praveen Phatak <praveenphatak@gmail.com>
2021-10-22 12:55:23 +0530

    Settings changed

--- project_worldcup/settings/production.py
+++ project_worldcup/settings/production.py
@@ -13,5 +13,6 @@ else:
     DEBUG = False
     ALLOWED_HOSTS = ['t20-wc-2021.herokuapp.com']
     db_from_env = dj_database_url.config(conn_max_age=600)
+    DATABASES = {'default': {}}
     DATABASES['default'].update(db_from_env)
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
